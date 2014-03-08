e3ec3ac51557ee0e0cec14479ae27a9c0cddc9f2
Adam Tuttle <adam@fusiongrokker.com>
2014-03-08 00:25:48 -0500

    requeue using a deep clone of the item

--- corq.js
+++ corq.js
@@ -167,7 +167,7 @@
 	}
 
 	function $requeue(item){
-		Q.push(item);
+		Q.push($clone(item));
 		$delete(item.id);
 	}
 
@@ -190,6 +190,39 @@
 			var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 			return v.toString(16);
 		});
+	}
+
+	function $clone(obj) {
+		// http://stackoverflow.com/a/728694/751
+		// Handle the 3 simple types, and null or undefined
+		if (null == obj || "object" != typeof obj) return obj;
+
+		// Handle Date
+		if (obj instanceof Date) {
+			var copy = new Date();
+			copy.setTime(obj.getTime());
+			return copy;
+		}
+
+		// Handle Array
+		if (obj instanceof Array) {
+			var copy = [];
+			for (var i = 0, len = obj.length; i < len; i++) {
+				copy[i] = $clone(obj[i]);
+			}
+			return copy;
+		}
+
+		// Handle Object
+		if (obj instanceof Object) {
+			var copy = {};
+			for (var attr in obj) {
+				if (obj.hasOwnProperty(attr)) copy[attr] = $clone(obj[attr]);
+			}
+			return copy;
+		}
+
+		throw new Error("Unable to copy obj! Its type isn't supported.");
 	}
 
 	return corq;
